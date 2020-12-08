9e55d5e7f2da02ee6e680a21952dfef59808c084
AustinJacobs <aust.t.jac@gmail.com>
2020-12-07 20:20:28 -0700

    Finished section about Jesus Christ

---
+++ Religion Project/js/main.js
@@ -0,0 +1,6 @@
+let date = new Date();
+let year = date.getFullYear()
+
+document.getElementById("copyright-year").innerHTML = year;
+
+console.log(year)
--- script.js
+++ script.js
@@ -41,3 +41,4 @@
 
 document.getElementById("last-update").innerHTML = document.lastModified;
 document.getElementById("copyright-year").innerHTML = (new Date().getFullYear());   
+

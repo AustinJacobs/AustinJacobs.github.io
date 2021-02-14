ab9d71cdcbf57f99a9a5ce139adf9cb4849d1d41
AustinJacobs <aust.t.jac@gmail.com>
2021-02-14 13:41:16 -0700

    save

--- nav.js
+++
@@ -1,8 +0,0 @@
-function toggleRightMenu() {
-    navMenuStatus = document.getElementById('rightMenu').style.display;
-    if ( navMenuStatus == 'none' ) {
-      document.getElementById('rightMenu').style.display = 'block';
-    } else {
-      document.getElementById('rightMenu').style.display = 'none';
-    }
-  }
--- script.js
+++
@@ -1,5 +0,0 @@
-let d = new Date();
-let n = d.getFullYear();
-document.getElementById("current-year").textContent = n;
-
-

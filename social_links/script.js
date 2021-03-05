7c7439ab564e0e01b772000e085fa1fac44a0234
AustinJacobs <aust.t.jac@gmail.com>
2021-03-05 16:35:18 -0700

    save

--- social_links/script.js
+++ social_links/script.js
@@ -2,3 +2,7 @@ let date = new Date();
 let year = date.getFullYear();
 
 document.getElementById("date").innerHTML = year;
+
+let x = document.lastModified;
+
+document.getElementById("update").innerHTML = x;

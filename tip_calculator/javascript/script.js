b5b0e6307733d36fa183c261f834ec52b11bb8e3
AustinJacobs <aust.t.jac@gmail.com>
2021-08-11 21:05:01 -0600

    Need to fix tip button values. Working except this part.

--- tip_calculator/javascript/script.js
+++ tip_calculator/javascript/script.js
@@ -1,4 +1,20 @@
 function tipCalc() {
     let bill = document.getElementById('bill').value;
-    let 
+    let val1 = document.getElementById('one').value;
+    let val2 = document.getElementById('two').value;
+    let val3 = document.getElementById('three').value;
+    let val4 = document.getElementById('four').value;
+    let val5 = document.getElementById('five').value;
+    let val6 = document.getElementById('six').value;
+    let people = document.getElementById('num_people').value;
+
+    let bill_before = bill * val3;
+
+    let bill_after = Math.floor(bill_before / people * 100) / 100;
+
+    let total = (bill / people) + bill_after;
+
+    document.getElementById('no').innerHTML = '$' + bill_after;
+
+    document.getElementById('yes').innerHTML = '$' + total.toFixed(2);
 }

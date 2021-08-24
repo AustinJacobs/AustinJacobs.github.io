9479c3abe2abedb11feeddf5fdfac91f508712cc
AustinJacobs <aust.t.jac@gmail.com>
2021-08-23 21:26:21 -0600

    started sunnyside. Got mobile nav working.

---
+++ sunnyside/js/script.js
@@ -0,0 +1,8 @@
+function navFunction() {
+    let x = document.getElementById("nav");
+    if(x.style.display === "block") {
+        x.style.display = "none";
+     } else {
+         x.style.display = "block";
+     }
+}

880a37a0779e629f63995f80eb5dce1087f6b58d
AustinJacobs <aust.t.jac@gmail.com>
2021-08-24 20:36:55 -0600

    save

--- sunnyside/js/script.js
+++ sunnyside/js/script.js
@@ -1,8 +1,15 @@
 function navFunction() {
     let x = document.getElementById("nav");
+    let y = document.getElementById("egg-picture");
     if(x.style.display === "block") {
         x.style.display = "none";
      } else {
          x.style.display = "block";
+     }
+
+     if(x.style.display === "block") {
+         y.style.marginTop = "-5.28em";
+     } else {
+         y.style.marginTop = "16em";
      }
 }

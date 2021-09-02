1ccc930738908d3bcec05cb5735742da51ca31bd
AustinJacobs <aust.t.jac@gmail.com>
2021-09-01 19:22:58 -0600

    save

--- sunnyside/js/script.js
+++
@@ -1,9 +0,0 @@
-function navFunction() {
-    let x = document.getElementById("nav");
-    let y = document.getElementById("egg-picture");
-    if(x.style.display === "block") {
-        x.style.display = "none";
-     } else {
-         x.style.display = "block";
-     }
-}

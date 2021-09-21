354f482f80c6f954b636db20dca02d154a7fbd90
AustinJacobs <aust.t.jac@gmail.com>
2021-09-21 15:21:07 -0600

    save

--- programming_with_classes/tic_game.py
+++ programming_with_classes/tic_game.py
@@ -1,20 +1,56 @@
 # Assignment Tic, Tac, Toe Game. Completed by Austin Jacobs
 
-board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+def main():
+    player = next_player("")
+    board = create_board()
+    while not (is_winner(board) or is_draw(board)):
+        display_board(board)
+        make_move(player, board)
+        player = next_player(player)
+    display_board(board)
+    print("Good game. Thanks for playing!") 
 
+def create_board():
+    board = []
+    for square in range(9):
+        board.append(square + 1)
+    return board
 
-
-# def createBoard():
-#     a = (' |' *  2 )
-#     c = ''.join('-+-+-')
-#     print('\n'.join((a, c, a, c, a)))
-
-# createBoard()
-
-
-def displayBoard(board):
-    row_length = 3
-    for i in range(0, len(board), row_length):
-        print('|'.join(board[i:i+row_length]))
+def display_board(board):
+    print()
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print('-+-+-')
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print('-+-+-')
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+    print()
     
-displayBoard(board)
+def is_draw(board):
+    for square in range(9):
+        if board[square] != "x" and board[square] != "o":
+            return False
+    return True 
+    
+def is_winner(board):
+    return (board[0] == board[1] == board[2] or
+            board[3] == board[4] == board[5] or
+            board[6] == board[7] == board[8] or
+            board[0] == board[3] == board[6] or
+            board[1] == board[4] == board[7] or
+            board[2] == board[5] == board[8] or
+            board[0] == board[4] == board[8] or
+            board[2] == board[4] == board[6])
+
+def make_move(player, board):
+    square = int(input(f"{player}'s turn to choose a square (1-9): "))
+    board[square - 1] = player
+
+
+def next_player(current):
+    if current == "" or current == "o":
+        return "x"
+    elif current == "x":
+        return "o"
+
+if __name__ == "__main__":
+    main()

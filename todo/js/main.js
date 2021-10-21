53b63418f78945a21df436e7fdf71059220aaaa4
AustinJacobs <aust.t.jac@gmail.com>
2021-10-21 08:56:17 -0600

    pushing updated todo changes

--- todo/js/main.js
+++ todo/js/main.js
@@ -1,4 +1,4 @@
-const list_of_todos = [];
+let list_of_todos = JSON.parse(localStorage.getItem("list_of_todos") || "[]");
 
 class TodoTask {
     constructor(item) {
@@ -8,7 +8,7 @@ class TodoTask {
     addTask() {
         let task_text = document.querySelector("#user_task_text").value;
         if (task_text == "") {
-            alert("You did not enter any item!")
+            alert("Please enter a task!")
         } else {
             const todoObject = {
                 id: list_of_todos.length,
@@ -24,19 +24,17 @@ class TodoTask {
 
     done_undone(x) {
         const selectedTodoIndex = list_of_todos.findIndex((item) => item.id == x);
-        console.log(list_of_todos[selectedTodoIndex].isDone);
         list_of_todos[selectedTodoIndex].isDone == false ? list_of_todos[selectedTodoIndex].isDone = true : list_of_todos[selectedTodoIndex].isDone = false;
         this.display();
     }
 
-    deleteElement(z) {
+    deleteTask(z) {
         const selectedDelIndex = list_of_todos.findIndex((item) => item.id == z);
 
         list_of_todos.splice(selectedDelIndex, 1);
 
         this.display();
     }
-
 
     display() {
         this.primaryElement.innerHTML = "";
@@ -46,15 +44,16 @@ class TodoTask {
             const divElement = document.createElement("div");
             const delete_button = document.createElement("button");
             const checkbox = document.createElement("input");
-            const paragraph = document.createElement("p");
+            const span = document.createElement("span");
 
-            paragraph.setAttribute("class", "todos_words");
+            span.setAttribute("class", "todos_words");
 
             checkbox.type = "checkbox";
             checkbox.setAttribute("class", "checkboxes");
+            checkbox.setAttribute("data-id", object_item.id);
 
-            paragraph.innerHTML = object_item.todoText;
-            paragraph.setAttribute("data-id", object_item.id);
+            span.innerHTML = object_item.todoText;
+            span.setAttribute("data-id", object_item.id);
             divElement.setAttribute("class", "todos_div");
 
             delete_button.setAttribute("data-id", object_item.id);
@@ -62,21 +61,27 @@ class TodoTask {
             delete_button.innerHTML = "X";
 
             divElement.appendChild(checkbox);
-            divElement.appendChild(paragraph);
+            divElement.appendChild(span);
             divElement.appendChild(delete_button);
-            
+
             delete_button.addEventListener("click", function (e) {
                 const deleteId = e.target.getAttribute("data-id");
-                UserTodoList.deleteElement(deleteId);
+                UserTodoList.deleteTask(deleteId);
             })
 
-            divElement.addEventListener("click", function (e) {
+            span.addEventListener("click", function (e) {
+                const selectedId = e.target.getAttribute("data-id");
+                UserTodoList.done_undone(selectedId);
+            })
+
+            checkbox.addEventListener("click", function (e) {
                 const selectedId = e.target.getAttribute("data-id");
                 UserTodoList.done_undone(selectedId);
             })
 
             if (object_item.isDone) {
-                paragraph.classList.add("checked");
+                span.classList.add("checked");
+                checkbox.checked = true;
             }
 
             let ulAppend = document.querySelector(".todos");
@@ -86,13 +91,20 @@ class TodoTask {
             let tasksLeft = document.getElementById("items_left");
             tasksLeft.innerHTML = list_of_todos.length;
         })
+
+        localStorage.setItem("list_of_todos", JSON.stringify(list_of_todos));
+
+        let tasksLeft = document.getElementById("items_left");
+        if (list_of_todos.length == 0) {
+            tasksLeft.innerHTML = "0";
+        }
     }
 }
 
-////-----MAIN PROGRAM------------
-const listSection = document.querySelector(".todos");
+const taskSection = document.querySelector(".todos");
 
-UserTodoList = new TodoTask(listSection);
+UserTodoList = new TodoTask(taskSection);
+UserTodoList.display()
 
 document.querySelector("#add_button").addEventListener("click", function () {
     UserTodoList.addTask()
@@ -101,5 +113,36 @@ document.querySelector("#add_button").addEventListener("click", function () {
 document.querySelector("#user_task_text").addEventListener("keydown", function (e) {
     if (e.keyCode == 13) {
         UserTodoList.addTask()
+    }
+});
+
+const all = document.getElementById("all");
+const active = document.getElementById("active");
+const completed = document.getElementById("completed");
+
+all.addEventListener("click", function () {
+    all.setAttribute("class", "on");
+    if (active.classList.contains("on")) {
+        active.classList.remove("on");
+    } else if (completed.classList.contains("on")) {
+        completed.classList.remove("on");
+    }
+})
+
+active.addEventListener("click", function () {
+    active.setAttribute("class", "on");
+    if (all.classList.contains("on")) {
+        all.classList.remove("on");
+    } else if (completed.classList.contains("on")) {
+        completed.classList.remove("on");
+    }
+})
+
+completed.addEventListener("click", function () {
+    completed.setAttribute("class", "on");
+    if (all.classList.contains("on")) {
+        all.classList.remove("on");
+    } else if (active.classList.contains("on")) {
+        active.classList.remove("on");
     }
 })

070807888d638c0652ad9d9e73e1d884a51a6b4a
Austin Jacobs <aust.t.jac@gmail.com>
2022-01-27 19:57:30 -0700

    saved new exercise

--- js-basics/index.js
+++ js-basics/index.js
@@ -19,15 +19,34 @@
 
 // Exercise #3
 
-const output = fizzBuzz(8);
-console.log(output);
+// const output = fizzBuzz(8);
+// console.log(output);
 
-function fizzBuzz(input) {
-    if (typeof input !== 'number')
-        return NaN;
-    if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz";
-    if (input % 3 === 0) return "Fizz";
-    if (input % 5 === 0) return "Buzz";
-    return input;
+// function fizzBuzz(input) {
+//     if (typeof input !== 'number')
+//         return NaN;
+//     if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz";
+//     if (input % 3 === 0) return "Fizz";
+//     if (input % 5 === 0) return "Buzz";
+//     return input;
 
+// }
+
+checkSpeed(130);
+
+function checkSpeed(speed) {
+    const speedLimit = 70;
+    const mphPoint = 5;
+
+    if (speed <= speedLimit + mphPoint) {
+        console.log('OK')
+    } else {
+        let points = Math.floor((speed - speedLimit) / mphPoint);
+        if (points >= 12) {
+            console.log("License Suspended");
+        }
+        else {
+            console.log('Points', points)
+        }
+    }
 }

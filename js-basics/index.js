1c24cd79cc4532774a0fc1b1271e14a4fd0ed9c1
Austin Jacobs <aust.t.jac@gmail.com>
2022-02-08 22:06:05 -0700

    Interview Preparation

--- js-basics/index.js
+++ js-basics/index.js
@@ -32,21 +32,90 @@
 
 // }
 
-checkSpeed(130);
+// checkSpeed(130);
 
-function checkSpeed(speed) {
-    const speedLimit = 70;
-    const mphPoint = 5;
+// function checkSpeed(speed) {
+//     const speedLimit = 70;
+//     const mphPoint = 5;
 
-    if (speed <= speedLimit + mphPoint) {
-        console.log('OK')
-    } else {
-        let points = Math.floor((speed - speedLimit) / mphPoint);
-        if (points >= 12) {
-            console.log("License Suspended");
-        }
-        else {
-            console.log('Points', points)
-        }
-    }
-}
+//     if (speed <= speedLimit + mphPoint) {
+//         console.log('OK')
+//     } else {
+//         let points = Math.floor((speed - speedLimit) / mphPoint);
+//         if (points >= 12) {
+//             console.log("License Suspended");
+//         }
+//         else {
+//             console.log('Points', points)
+//         }
+//     }
+// }
+
+// const numbers = [1, 2, 3, 1, 4];
+
+// // End
+// numbers.push(5, 6)
+
+// // Beginning 
+// numbers.unshift(1, 2);
+
+
+// // Middle
+// numbers.splice(2, 0, 'a', 'b');
+
+// console.log(numbers)
+
+// console.log(numbers.indexOf('a'));
+
+// console.log(numbers.lastIndexOf(1));
+
+// console.log(numbers.indexOf(1) !== -1);
+// console.log(numbers.includes(1));
+
+// let numbers = [1, 2, 3, 4, 5, 6];
+// // push 
+// numbers.push(7);
+// console.log(numbers);
+// // pop
+// numbers.pop()
+// console.log(numbers);
+
+// //unshift
+// numbers.unshift(0);
+// console.log(numbers);
+// // shift
+// numbers.shift();
+// console.log(numbers);
+
+// //splice
+// numbers.splice(2, 1);
+// console.log(numbers);
+
+// numbers.splice(2, 0, 3);
+// console.log(numbers);
+
+// //map
+// let addTwo = numbers.map((number) => {
+//     return number + 2;
+// });
+
+// console.log(addTwo);
+
+// // filter
+// let filteredEvens = numbers.filter((number) => {
+//     return number > 3;
+// });
+
+// console.log(filteredEvens);
+
+// //forEach
+// let addedWords = numbers.forEach((number) => {
+//     console.log(number + 2);
+// });
+
+// console.log(addedWords);
+
+// //for
+// for (i = 0; i < numbers.length; i++) {
+//     console.log(numbers);
+// }

701b5d6369e4c78a62bfaea63dc5eed89e2ade4a
Austin Jacobs <aust.t.jac@gmail.com>
2022-03-08 16:11:56 -0700

    Save

--- js-basics/index.js
+++ js-basics/index.js
@@ -119,3 +119,12 @@
 // for (i = 0; i < numbers.length; i++) {
 //     console.log(numbers);
 // }
+
+// showNumbers(10);
+
+// function showNumbers(limit) {
+//   for (let i = 0; i <= limit; i++) {
+//     const message = (i % 2 === 0) ? 'EVEN' : 'ODD';
+//     console.log(i, message);
+//   }
+// }
--- js-basics/objects/1-maxOfTwo.js
+++ js-basics/objects/1-maxOfTwo.js
@@ -1,7 +1,14 @@
 
-let numbet = max(1, 2);
+// let number = max(1, 2);
+// console.log(number);
+
+// function max(a, b) { 
+//   return (a > b) ? a : b;
+// }
+
+let number = maxOfTwo(100, 3);
 console.log(number);
 
-function max(a, b) { 
-  return (a > b) ? a : b;
+function maxOfTwo(numberOne, numberTwo) {
+  return (numberOne > numberTwo) ? numberOne : numberTwo;
 }

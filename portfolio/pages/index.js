d72b8818ff8ef3b67d79c95765ec9931a464016f
Austin Jacobs <aust.t.jac@gmail.com>
2022-05-18 22:45:52 -0600

    trimmed down styles

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -20,16 +20,15 @@ const App = () => {
     <div>
       <Grid.Container alignItems='center' gap={2} css={{ p: 20 }}>
         <Grid fluid={2}>
-          <Text size='2em' color='primary' weight='bold'>
+          <Text size='2em' weight='bold'>
             Austin Jacobs
           </Text>
         </Grid>
-        <Grid fluid={1} justify='right'>
+        <Grid fluid={1}>
           <Switch
             bordered
             checked={isDark}
             onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
-            color='primary'
             size='lg'
             iconOn={<SunIcon filled />}
             iconOff={<MoonIcon filled />}
@@ -37,15 +36,9 @@ const App = () => {
         </Grid>
       </Grid.Container>
       <Grid.Container>
-        <Card color='primary'>
-          <Row justify='center' align='center'>
-            <Text h6 size={15} color='white' css={{ m: 0 }}>
-              NextUI gives you the best developer experience with all the
-              features you need for building beautiful and modern websites and
-              applications.
-            </Text>
-          </Row>
-        </Card>
+        <Text>
+          Hello
+        </Text>
       </Grid.Container>
     </div>
   );
